40a8b4f468f18fa2e86e64e8efa425ad3ac4450a
shahinawab1814-oss <shahinawab1814@gmail.com>
2026-08-24 05:10:08 +0100

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// WhisperVault Firebase Web SDK configuration.
+// This is safe client configuration. NEVER place a Firebase Admin private key here.
+const firebaseConfig = {
+  apiKey: "AIzaSyAkEnauvyGiGTgtYdPpwmeD3Rq9JObYquc",
+  authDomain: "whispervault-3588e.firebaseapp.com",
+  projectId: "whispervault-3588e",
+  storageBucket: "whispervault-3588e.firebasestorage.app",
+  messagingSenderId: "368750141209",
+  appId: "1:368750141209:web:652a04a44149d12a616f88",
+  measurementId: "G-G247RKBEQT"
+};
